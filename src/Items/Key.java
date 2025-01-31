983d689a03288e2b1ad72d6e8ba3336189fd1f51
James Ellison <jamesellison95@gmail.com>
2025-01-31 10:55:34 +0100

    UpdateContainer
    --

--- src/Items/Key.java
+++ src/Items/Key.java
@@ -2,9 +2,14 @@ package Items;
 import Main.*;
 
 public class Key extends Item{
-    private int ID;
+    private final int ID;
 
     public Key(String name, String description, Container aContainer){
         super(name, description, Weight.KEY.getWeight());
+        ID = aContainer.getID();
+    }
+
+    public int getID(){
+        return ID;
     }
 }
